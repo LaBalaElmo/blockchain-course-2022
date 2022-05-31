6e24039f6023db5fe09b295eeee4b1dfcfebd7ec
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-05-31 13:20:03 -0400

    tarea 2

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,10 +7,20 @@ contract('Inbox', accounts => {
         assert.equal(message, 'Hi')
     });
 
-    it('setMessage', async () => {
+    it('setMessage should change the message', async () => {
         const instance = await Inbox.deployed();
         await instance.setMessage('Hi Rodrigo', {from: accounts[0]});
         const message = await instance.getMessage.call();
         assert.equal(message, 'Hi Rodrigo');
+    });
+
+    it('setMessage shouldnt chage the message', async () => {
+        const instance = await Inbox.deployed();
+        try {
+            await instance.setMessage('Second change', {from: accounts[1]});
+        }catch (e) {
+            //console.log('error', e);
+            assert.equal(e.reason, "Solo el owner puede modificar el mensaje")
+        }
     });
 })
