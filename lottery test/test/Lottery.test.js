d2e1e2e73cd7e533434e7032f5b6d0dd88afbba5
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-06-08 12:41:35 -0400

    [08-06-2022] Tarea1

--- lottery test/test/Lottery.test.js
+++ lottery test/test/Lottery.test.js
@@ -1,11 +1,30 @@
 const Lottery = artifacts.require("Lottery");
 
 contract("lottery test", accounts => {
+
+    let instance;
+
+    beforeEach("Before each", async () => {
+        instance = await Lottery.new();
+    })
+
     it("allows one account to enter", async () => {
-        const instance = await Lottery.deployed();
         await instance.enter({from: accounts[1], value: web3.utils.toWei("3", "ether")});
         const players = await instance.getPlayers.call();
         assert.equal(accounts[1], players[0]);
         assert.equal(1, players.length);
+    })
+
+    it("allows multiple account to enter", async ()=> {
+        await instance.enter({from: accounts[0], value: web3.utils.toWei("3", "ether")});
+        await instance.enter({from: accounts[1], value: web3.utils.toWei("4", "ether")});
+        await instance.enter({from: accounts[2], value: web3.utils.toWei("5", "ether")});
+
+        const players = instance.getPlayers.call();
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equals(players.length, 3);
     })
 })
