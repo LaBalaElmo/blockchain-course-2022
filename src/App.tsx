5313288807b416aa54b974fc98904dbf3339f12e
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-06-13 13:41:54 -0400

    task1

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { connectWallet, initialize } from './ethereum/web3';
+import contractLottery from "./ethereum/abis/Lottery.json";
 
 function App() {
   /*
@@ -15,6 +16,26 @@ function App() {
     }
   }, []);
 
+  const loadBlockChainData = async () => {
+
+    //@ts-ignore
+    const Web3 = window.web3;
+    const networkData = contractLottery.networks["5777"];
+    console.log('networkData', networkData)
+
+    if(networkData){
+      const abi = contractLottery.abi;
+      const address = networkData.address;
+      console.log('address', address);
+
+      const contractDeployed = new Web3.eth.Contract(abi, address);
+
+      const players = await contractDeployed.methods.getPlayers().call();
+      console.log('players', players, players.length);
+    }
+    //Rinkeby 4, Ganache 5777, BSC 97
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,6 +45,7 @@ function App() {
         </p>
         <p>Hi React, Truffle, Firebase</p>
         <button onClick={()=> connectWallet()}>Connect</button>
+        <button onClick={()=> loadBlockChainData()}>Load</button>
       </header>
     </div>
   );
