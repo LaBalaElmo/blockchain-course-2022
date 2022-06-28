9c8fc8641d996dcfa5743bf45d8cd2e369dc5d4a
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-06-28 14:11:04 -0400

    [28-06-2022] task4 mandatory methods ERC20

--- src/ethereum/contracts/ERC20.sol
+++ src/ethereum/contracts/ERC20.sol
@@ -34,11 +34,34 @@ contract ERC20 is IERC20, IERC20Metadata{
     //event Transfer(address indexed from, address indexed to, uint256 value);
     //event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    //function totalSupply() external view returns(uint256);
-    //function balanceOf(address account) external view returns(uint256);
-    //function transfer(address to, uint256 amount) external returns(bool);
-    //function allowance(address owner, address spender) external view returns(uint256);
-    //function approve(address spender, uint256 amount) external returns(bool);
-    //function transferFrom(address from, address to, uint256 amount) external returns(bool);
+    function totalSupply() public view override returns(uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns(uint256){
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 amount) public override returns(bool){
+        address owner = msg.sender;
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns(uint256){
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns(bool){
+        address owner = msg.sender;
+        _approve(owner, spender, amount);
+        return true;
+    }
+    function transferFrom(address from, address to, uint256 amount) public override returns(bool){
+        address spender = msg.sender;
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
 
 }
